208dd5d0abaa862f4795b7efaf5b919b3cb556cf
Nico Baier <nico.baier@gmail.com>
2025-10-04 00:05:58 -0500

    feat: added alchemy

---
+++ alchemy.run.ts
@@ -0,0 +1,19 @@
+/// <reference types="@types/node" />
+
+import alchemy from "alchemy";
+import { Vite } from "alchemy/cloudflare";
+import { CloudflareStateStore } from "alchemy/state";
+
+const app = await alchemy("csv-analyzer", {
+	stateStore: (scope) => new CloudflareStateStore(scope),
+});
+
+export const worker = await Vite("app", {
+	entrypoint: "src/worker.ts",
+});
+
+console.log({
+	url: worker.url,
+});
+
+await app.finalize();
